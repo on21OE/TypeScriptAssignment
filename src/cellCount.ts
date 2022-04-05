edf5bf48f33f1f08da875b8fc92c9baf617cd900
on21OE <oli.ebert.21@lehre.mosbach.dhbw.de>
2022-04-05 23:34:32 +0200

    Added currentWord

--- src/cellCount.ts
+++ src/cellCount.ts
@@ -1,22 +1,28 @@
-import { fillCellWithLetter } from "./index";
+import { fillCellWithLetter, getCurrentWord } from "./index";
 import { enter, del, letters, cell00 } from "./inputs";
 
 export let colCount = 0;
 export let rowCount = 0;
 export let cellCount = colCount.toString() + rowCount.toString();
 
+let currentWord: string = "";
+
 function updateCellCount() {
     cellCount = rowCount.toString() + colCount.toString();
-    console.log(cellCount);
+    // console.log(cellCount);
 }
 
 export function updateRowCount() {
     enter.onclick = function() {
       if (colCount === 4) {
+        for (let i = 0; i < 5; i++) {
+          currentWord = currentWord + document.getElementById("cell" + rowCount.toString() + i)!.innerHTML;
+          console.log(currentWord);
+        }
         rowCount = rowCount < 5 ? rowCount + 1: rowCount;
         colCount = 0;
       }
-      console.log(cellCount);
+      // console.log(cellCount);
       updateCellCount();
     }
   }
@@ -24,16 +30,16 @@ export function updateRowCount() {
   export function updateColCount() {
     del.onclick = function() {
       colCount = colCount > 0 ? colCount - 1: colCount;
-      console.log("delete", cellCount);
+      // console.log("delete", cellCount);
     }
   
     for (let letter of letters) {
       letter.onclick = function() {
         fillCellWithLetter(letter.id, cellCount);
         colCount = colCount < 4 ? colCount + 1: colCount;
-        document.getElementById("a") as HTMLButtonElement;
-        console.log(letter.id);
+        // console.log(letter.id);
         updateCellCount();
       }
     }
   }
+
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 //THIS IS THE ENTRY FILE - WRITE YOUR MAIN LOGIC HERE!
 
-import { cellCount, updateColCount, updateRowCount } from "./cellCount";
+import { cellCount, colCount, rowCount, updateColCount, updateRowCount } from "./cellCount";
+import { enter } from "./inputs";
 
+const correctWord = "apfel";
+
+let currentWord: string[] = [];
 
 updateColCount();
 updateRowCount();
@@ -9,6 +13,25 @@ updateRowCount();
 export function fillCellWithLetter(letter: string, cellCount: string) {
     document.getElementById("cell" + cellCount)!.innerHTML = letter;
 };
+
+
+export function getCurrentWord() {
+    console.log(document.getElementById("cell0" + colCount)!.innerHTML);
+    if (rowCount === 0 && currentWord.length < 5) {
+       // currentWord.push(document.getElementById("cell0" + (colCount - 1))!.innerHTML);
+        console.log(currentWord);
+    }
+    enter.onclick = function() {
+        if (colCount === 4) {
+          
+        }
+        // console.log(cellCount);
+      }
+}
+
+
+
+
 
 
 
