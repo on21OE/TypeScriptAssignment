31ae9b149ea38145b401a86dcfc9c308432f3cdd
on21OE <oli.ebert.21@lehre.mosbach.dhbw.de>
2022-04-07 23:09:16 +0200

    add css styling

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
 import { cellCount, colCount, rowCount, updateColCount, updateRowCount } from "./cellCount";
 import { enter } from "./inputs";
 
-const wordList: string[] = ["apfel", "pappe", "klein"]
+const wordList: string[] = ["apfel", "pappe", "klein", "feder"]
 const correctWord = wordList[Math.floor(Math.random() * wordList.length)];
 const correctWordArray = correctWord.split("");
 
