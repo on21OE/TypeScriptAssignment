f99c53244fa2a3df5907df1005eb68009440585e
on21OE <oli.ebert.21@lehre.mosbach.dhbw.de>
2022-04-13 18:36:42 +0200

    color changes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { cellCount, colCount, rowCount, updateColCount, updateRowCount } from "./cellCount";
 import { enter } from "./inputs";
 
-const wordList: string[] = ["feder"]
+const wordList: string[] = ["mauer"]
 const correctWord = wordList[Math.floor(Math.random() * wordList.length)];
 const correctWordArray = correctWord.split("");
 
@@ -12,7 +12,7 @@ updateRowCount();
 markCurrentCell();
 
 export function fillCellWithLetter(letter: string, cellCount: string) {
-    document.getElementById("cell" + cellCount)!.innerHTML = letter.toLocaleUpperCase();
+    document.getElementById("cell" + cellCount)!.innerHTML = letter;
 };
 
 // function markCurrentRow() {
@@ -30,22 +30,96 @@ export function getCurrentWord() {
 }
 
 export function compareWords() {
+    if (currentWordArray === correctWordArray) {
+        checkForWinner();
+    }
+
+    // if (currentWordArray.length !== new Set(currentWordArray).size) {
+    //   console.log(new Set(currentWordArray).size)
+    // }
+
+    // const findDuplicates = (currentWordArray: any[]) => currentWordArray.filter((item, index) => currentWordArray.indexOf(item) !== index)
+    // const duplicatesOfCurrentWord = findDuplicates(currentWordArray);
+    // console.log(duplicatesOfCurrentWord);
+
+
+
+
     for (let i = 0; i < 5; i++) {
-        if (currentWordArray === correctWordArray) {
-            checkForWinner();
-        }
         if (currentWordArray[i] === correctWordArray[i]) {
             console.log("Dein Buchstabe " + currentWordArray[i] + " kommt genau an der " + (i + 1) + ". Stelle vor!")
-            document.getElementById("cell" + rowCount.toString() + i)!.style.backgroundColor = "#C5FFA5";
+
+            document.getElementById("cell" + rowCount.toString() + i)!.style.backgroundColor = "rgba(75, 87, 62, 0.8)";
+            document.getElementById("cell" + rowCount.toString() + i)!.style.color = "#FFFFFF";
         } else {
             for (let j = 0; j < 5; j++) {
                 if (currentWordArray[i] === correctWordArray[j]) {
                     console.log("Dein Buchstabe " + currentWordArray[i] + " kommt irgendwo im Wort vor!")
-                    document.getElementById("cell" + rowCount.toString() + i)!.style.backgroundColor = "#F9F871";
+                    document.getElementById("cell" + rowCount.toString() + i)!.style.backgroundColor = "rgba(197, 166, 46, 0.7)";
+                    document.getElementById("cell" + rowCount.toString() + i)!.style.color = "#FFFFFF";
                 }
             }
         }
     }
+
+    let lettersToCheck: string[] = [];
+    for (let i = 0; i < 5; i++) {
+        let guessedLetterCount: number = 0;
+        if (currentWordArray[i] === correctWordArray[i]) {
+            document.getElementById("cell" + rowCount.toString() + i)!.style.backgroundColor = "rgba(75, 87, 62, 0.8)";
+            document.getElementById("cell" + rowCount.toString() + i)!.style.color = "#FFFFFF";
+            guessedLetterCount++;
+        } else {
+            lettersToCheck.push(currentWordArray[i])
+        }
+        console.log(lettersToCheck);
+        
+    }
+
+    // let currentWord = currentWordArray.join("");
+    // let duplicatesOfCurrentWord: string[] = [];
+    // let allDuplicatesOfCurrentWord: string[] = [];
+    // let duplicatesOfCorrectWord: string[] = [];
+    // let allDuplicatesOfCorrectWord: string[] = [];
+
+    // for (const letter of currentWordArray) {
+    //     const splittedWord = currentWord.split(letter);
+    //     console.log(splittedWord);
+    //     if (splittedWord.length === 2) {
+    //         console.log("Yeah, kein Duplikat");
+    //     }
+    //     if (splittedWord.length > 2) {
+    //         console.log("Dopplung")
+    //         allDuplicatesOfCurrentWord.push(letter);
+    //         if (duplicatesOfCurrentWord.indexOf(letter) === -1) {
+    //             duplicatesOfCurrentWord.push(letter);
+    //         }
+    //     }
+    // }
+
+    // for (const letter of correctWordArray) {
+    //     const splittedWord = correctWord.split(letter);
+    //     if (splittedWord.length === 2) {
+    //         console.log("Das Lösungswort enthält kein " + letter + " Dublikat");
+    //     }
+    //     if (splittedWord.length > 2) {
+    //         allDuplicatesOfCorrectWord.push(letter);
+    //         if (duplicatesOfCorrectWord.indexOf(letter) === -1) {
+    //             duplicatesOfCorrectWord.push(letter);
+    //         }
+    //     }
+    // }
+
+    // for (let i = 0; i < duplicatesOfCurrentWord.length; i++) {
+    //     for (let j = 0; j < duplicatesOfCorrectWord.length; j++)
+    //     if (duplicatesOfCurrentWord[i] === duplicatesOfCorrectWord[j]) {
+    //         console.log("Die beiden Arrays enthalten das selbe Duplikat " + duplicatesOfCurrentWord[i])
+            
+    //     } 
+    // }
+
+    
+
 }
 
 export function getCell(pRowCount: number, pColCount: number) {
